bd061ae1801730e43be92109713f7b90b67ff720
Even0311 <haoranzhi0311@gmail.com>
2021-12-14 23:20:38 +1100

    smokeball test

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,10 +1,23 @@
-const { httpGet } = require('./mock-http-interface');
+const { httpGet } = require("./mock-http-interface");
 
 const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
+	// TODO: Implement this function.
+	// return results;
+	return Promise.all(
+		urls.map(async (url) => {
+			try {
+				const response = await httpGet(url);
+				const theKey = response.status === 200 ? "Arnie Quote" : "FAILURE";
+				return { [theKey]: JSON.parse(response?.body).message };
+			} 
+      catch (err) {
+				// deal with error
+			}
+		})
+    
+	);
 };
 
 module.exports = {
-  getArnieQuotes,
+	getArnieQuotes,
 };
